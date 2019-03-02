c66d0c0d4db42d4ca9cc1852be6763eb6cf1a34f
grindlock <sergioenriqueperez@gmail.com>
2019-03-02 04:24:51 -0500

    Dear God it works!!! javascript wrapper is working

--- app/src/main/java/harmonize/com/harmonize/Login.java
+++ app/src/main/java/harmonize/com/harmonize/Login.java
@@ -2,12 +2,17 @@ package harmonize.com.harmonize;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    TextView temp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        temp = findViewById(R.id.test1);
+        temp.setText("Hello Sebastian");
     }
 }
