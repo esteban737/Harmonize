b8ae3e05914c0d59d0e5218f3cb4bae62e9e437c
esteban737 <perdomo.steven.77@knights.ucf.edu>
2019-03-02 18:09:22 -0500

    Authorization is functional and an Artist finder has been tested.

--- app/src/main/java/harmonize/com/harmonize/Login.java
+++ app/src/main/java/harmonize/com/harmonize/Login.java
@@ -7,22 +7,30 @@ import android.content.Intent;
 import android.util.Log;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Album;
+import kaaes.spotify.webapi.android.models.Artist;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 public class Login extends AppCompatActivity {
     TextView temp;
-    SpotifyApi api;
+
 
     private static final int REQUEST_CODE = 1337;
-    //private static final String CLIENT_ID = "960b9fef24394d88ac8280307294ffa0";
-    private static final String CLIENT_ID = "91a9588ff6134f01af5870fc49e8e39d";
-    private static final String REDIRECT_URI = "harmonizeapp://callback";
+    private static final String CLIENT_ID = "960b9fef24394d88ac8280307294ffa0";
+    //private static final String CLIENT_ID = "91a9588ff6134f01af5870fc49e8e39d";
+    private static final String REDIRECT_URI = "harmonize://callback";
 
     String ACCESS_CODE;
-     static String TOKEN;
+     static String TOKENS;
 
+    SpotifyApi api;
 
 
 
@@ -32,14 +40,40 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
         final AuthenticationRequest request = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI)
                 .setScopes(new String[]{"user-read-private", "playlist-read", "playlist-read-private", "streaming"})
                 .build();
 
 
         AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
-
     }
+
+
+    protected void getArtists() {
+
+        SpotifyService spotify = api.getService();
+
+        spotify.getArtist("0UWZUmn7sybxMCqrw9tGa7", new Callback<Artist>() {
+
+            @Override
+            public void success(Artist artist, Response response) {
+                Log.d("Artist success", artist.name);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d("Artist failure", error.toString());
+            }
+        });
+
+
+
+        }
+
+
+
+
 
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
@@ -56,9 +90,14 @@ public class Login extends AppCompatActivity {
                 // Response was successful and contains auth token
                 case TOKEN:
                     // Handle successful response
-                    String token = response.getAccessToken();
-                    temp.setText(token);
-                    Log.i("TEST", token);
+                    //TOKENS = response.getAccessToken();
+                    Log.d("TESTTT",response.getAccessToken());
+                    api = new SpotifyApi();
+                    api.setAccessToken(response.getAccessToken().trim());
+
+                    //Log.d("Testttt", )
+
+                    getArtists();
                     break;
 
                 // Auth flow returned an error
@@ -76,5 +115,9 @@ public class Login extends AppCompatActivity {
         }
     }
 
+    public void saveDocument(){
+
+
+    }
 
 }
