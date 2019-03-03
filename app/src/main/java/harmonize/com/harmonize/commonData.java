6b75de049fb0438f0102f1e5b59951551e2e0d12
esteban737 <perdomo.steven.77@knights.ucf.edu>
2019-03-02 19:57:00 -0500

    Started creating functions for acquiring data on common music choices.

---
+++ app/src/main/java/harmonize/com/harmonize/commonData.java
@@ -0,0 +1,89 @@
+package harmonize.com.harmonize;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class commonData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_common_data);
+    }
+
+    public void commonGenres(){
+
+
+
+
+    }
+
+
+    public void commonSongs(){
+
+
+    }
+
+
+
+    public void commonArtists({people}){
+        ArrayList<artists> artistList;
+
+
+        for (each person i = 0; i < n; i++){
+
+
+            for ( all favorite artists j = 0; j < m; j++){
+
+                new artists()
+
+                if(artistList.indexOf(favoriteArtists[j])) {
+
+                    artistsLists.add(new artists());
+                }
+
+                else{
+                    favoriteArtists[j].numofPeople++;
+                    favoriteArtists[j].overallPopularity += ranking;
+
+                }
+
+            }
+
+        }
+
+
+    }
+
+
+    public void artists(){
+
+        String names = "";
+        int numofPeople = 0;
+        int overallPopularity = 0;
+
+
+
+    }
+
+
+    public void genres(){
+
+        String title;
+        ArrayList<artists> artistList;
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
